e61fdb3916c9ad4267d0050d3c8250e031766f87
PRESSO <caff1nepill@gmail.com>
2023-06-28 00:21:40 +0900

    fix giscus bug

--- app/components/blog/Comment.tsx
+++ app/components/blog/Comment.tsx
@@ -1,24 +1,22 @@
-import Script from "next/script";
+'use client';
+
+import Giscus from "@giscus/react";
 
 const Comment = () => {
 	return (
 		<div className={'giscus'}>
-			<Script
-				src={"https://giscus.app/client.js"}
-		        data-repo={"pressogh/presso.codes"}
-		        data-repo-id={"R_kgDOI5m8jA"}
-		        data-category={"Comments"}
-		        data-category-id={"DIC_kwDOI5m8jM4CXH7I"}
-		        data-mapping={"pathname"}
-		        data-strict={"0"}
-		        data-reactions-enabled={"1"}
-		        data-emit-metadata={"0"}
-		        data-input-position={"top"}
-		        data-theme={"preferred_color_scheme"}
-		        data-lang={"ko"}
-		        data-loading={"lazy"}
-		        crossOrigin={"anonymous"}
-		        async
+			<Giscus
+				repo={"pressogh/presso.codes"}
+				repoId={"R_kgDOI5m8jA"}
+				category={"Comments"}
+				categoryId={"DIC_kwDOI5m8jM4CXH7I"}
+				mapping={"pathname"}
+				strict={"0"}
+				reactionsEnabled={"1"}
+				emitMetadata={"0"}
+				theme={"preferred_color_scheme"}
+				lang={"ko"}
+				loading={"lazy"}
 			/>
 		</div>
 	);
