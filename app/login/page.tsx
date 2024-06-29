0b6f30b40014b3f06e74e1ad61656f00d4bb4b5f
PRESSO <caff1nepill@gmail.com>
2024-06-29 17:34:15 +0900

    Feat: Change ui

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,21 +1,30 @@
 import { signIn } from "@/app/auth";
 
 import Container from "@/app/components/Container";
+import Github from "@/public/icons/Github";
+import Logo from "@/public/icons/Logo";
 
 const Page = () => {
 	return (
 		<Container>
-			<div className={`w-full h-[calc(100lvh-14rem)] min-h-96 flex justify-center items-center text-4xl font-light tracking-tighter`}>
+			<div className={`w-full h-[calc(100lvh-14rem)] min-h-96 flex justify-center items-center font-light`}>
 				<div
-					className={`max-w-xs w-full h-80 border rounded-md flex flex-col justify-center items-center gap-10`}>
-					<div>로그인</div>
+					className={`max-w-xs w-full h-80 border rounded-md flex flex-col items-center gap-10`}
+				>
+					<div className={`mt-24`}>
+						<Logo className={`size-20`} />
+					</div>
 					<form
 						action={async () => {
 							"use server"
 							await signIn("github");
 						}}
+						className={`mt-10 w-[86%] h-10 border py-2 rounded-md bg-[#303030] hover:bg-[#404040] transition-colors duration-300`}
 					>
-						<button type="submit">Login</button>
+						<button type="submit" className={`flex justify-center items-center text-white`}>
+							<Github className={`size-5 ml-4`} />
+							<div className={`ml-12`}>Github로 로그인</div>
+						</button>
 					</form>
 				</div>
 			</div>
