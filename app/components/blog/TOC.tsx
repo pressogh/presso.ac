0967ad3eb770e2f508a6ff027ff26b57c1eb1f37
PRESSO <caff1nepill@gmail.com>
2024-07-01 13:07:42 +0900

    Feat: Fix TOC UI

--- app/components/blog/TOC.tsx
+++ app/components/blog/TOC.tsx
@@ -11,17 +11,17 @@ interface Heading {
 
 const TOC = () => {
 	const [headings, setHeadings] = useState<Heading[]>([]);
-	
+
 	const activeId = useScrollSpy({
 		ids: headings.map(heading => heading.id),
 		options: {
 			rootMargin: '0% 0% -80% 0%',
 		},
 	});
-	
+
 	useEffect(() => {
 		if (typeof document === "undefined") return;
-		
+
 		const headings = Array.from(document.querySelectorAll<HTMLHeadingElement>("h3"));
 		const data = headings.map((heading) => {
 			return {
@@ -30,10 +30,10 @@ const TOC = () => {
 				tag: heading.tagName
 			}
 		});
-		
+
 		setHeadings(data);
 	}, []);
-	
+
 	return (
 		<aside className={`sticky top-20 xl:block hidden`}>
 			<div className={`absolute pt-0 w-60 overflow-hidden top-0 left-[calc(100%+3rem)]`}>
@@ -51,6 +51,7 @@ const TOC = () => {
 									text-[rgb(136,144,150)]
 									tracking-wide
 									${heading.id === activeId ? 'text-[rgb(75,110,160)]' : ''}
+									break-words
 								`}
 							>
 								<Link
@@ -81,14 +82,14 @@ interface HookProps {
 const useScrollSpy = ({ ids, options }: HookProps) => {
 	const [activeId, setActiveId] = useState<string | null>();
 	const observer = useRef<IntersectionObserver>();
-	
+
 	useEffect(() => {
 		const elements = ids.map(id => document.getElementById(`${id}`));
-		
+
 		if (observer.current) {
 			observer.current.disconnect();
 		}
-		
+
 		observer.current = new IntersectionObserver(entries => {
 			entries.forEach(entry => {
 				if (entry.isIntersecting) {
@@ -96,10 +97,10 @@ const useScrollSpy = ({ ids, options }: HookProps) => {
 				}
 			});
 		}, options);
-		
+
 		elements.forEach(el => el && observer.current?.observe(el));
 		return () => observer.current?.disconnect();
 	}, [ids, options]);
-	
+
 	return activeId;
 };
